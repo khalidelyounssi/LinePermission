f7004581b80ac88cde68cdde7318e8990da2fc22
Khalid Elyounssi <elyounssiox2000@gmail.com>
2026-09-09 09:34:30 +0100

    feat: complete partie 1

--- src/main/java/ma/youcode/lineperm/service/UserService.java
+++ src/main/java/ma/youcode/lineperm/service/UserService.java
@@ -4,11 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 import org.mindrot.jbcrypt.BCrypt;
 import ma.youcode.lineperm.model.User;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.IOException;
 
 
 public class UserService{
 
     private final Map<String, User> users = new HashMap<>();
+
+    private final Path usersFile =Path.of("data", "users.txt");
+
+            public UserService() {
+                readUsers();
+            }
    
 
 
@@ -33,9 +44,10 @@ public class UserService{
 
         String passwordHash = BCrypt.hashpw(password, BCrypt.gensalt());
         
-        User user = new User(login , password);
+        User user = new User(login , passwordHash);
 
         users.put(login , user);
+        saveUsers();
 
         return true;
 
@@ -48,6 +60,11 @@ public class UserService{
         }
         return users.get(login);
     }
+
+
+
+
+
 
     public User authenticate(String login ,String password){
         if(login==null||password==null){
@@ -71,5 +88,64 @@ public class UserService{
         return user;
 
     }
+
+
+
+    public void saveUsers(){
+        List<String> lines = new ArrayList<>();
+       for(User user : users.values()){ 
+        String line = user.getLogin() +":"+user.getPasswordHash();
+
+         lines.add(line);
+
+        
+
+    }
+            try {
+            Files.createDirectories(usersFile.getParent());
+            Files.write(usersFile, lines);
+
+        } catch (IOException e) {
+            throw new RuntimeException(
+                    "Impossible de sauvegarder les utilisateurs.",e);
+        }
+    }
+
+    private void readUsers() {
+
+    if (!Files.exists(usersFile)) {
+        return;
+    }
+
+    try {
+        List<String> lines =
+                Files.readAllLines(usersFile);
+
+        for (String line : lines) {
+
+            if (line.trim().isEmpty()) {
+                continue;
+            }
+
+            String[] parts = line.split(":", 2);
+
+            if (parts.length != 2) {
+                continue;
+            }
+
+            String login = parts[0];
+            String passwordHash = parts[1];
+
+            User user =
+                    new User(login, passwordHash);
+
+            users.put(login, user);
+        }
+
+    } catch (IOException e) {
+        throw new RuntimeException(
+                "impossible de charger les user",e);
+    }
+}
     
 }
