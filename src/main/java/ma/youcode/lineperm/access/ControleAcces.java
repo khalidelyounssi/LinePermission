18b9c1f38826dc56e728950fc21a943d124b198a
Khalid Elyounssi <elyounssiox2000@gmail.com>
2026-09-10 16:06:29 +0100

    feat: access controle

--- src/main/java/ma/youcode/lineperm/access/ControleAcces.java
+++ src/main/java/ma/youcode/lineperm/access/ControleAcces.java
@@ -1,5 +1,64 @@
 package ma.youcode.lineperm.access;
 
+import ma.youcode.lineperm.model.FichierProtege;
+import ma.youcode.lineperm.model.User;
+
 public class ControleAcces {
-    
+
+    public static boolean isOwner(
+            User user,
+            FichierProtege fichier
+    ) {
+        if (user == null || fichier == null) {
+            return false;
+        }
+
+        return fichier.getOwner().equals(
+                user.getLogin()
+        );
+    }
+
+    public static boolean canAccess(
+            User user,
+            FichierProtege fichier,
+            char permission
+    ) {
+        if (user == null || fichier == null) {
+            return false;
+        }
+
+        permission =
+                Character.toLowerCase(permission);
+
+        if (isOwner(user, fichier)) {
+
+            switch (permission) {
+                case 'r':
+                    return fichier.isOwnerRead();
+
+                case 'w':
+                    return fichier.isOwnerWrite();
+
+                case 'd':
+                    return fichier.isOwnerDelete();
+
+                default:
+                    return false;
+            }
+        }
+
+        switch (permission) {
+            case 'r':
+                return fichier.isAutresRead();
+
+            case 'w':
+                return fichier.isAutresWrite();
+
+            case 'd':
+                return fichier.isAutresDelete();
+
+            default:
+                return false;
+        }
+    }
 }
